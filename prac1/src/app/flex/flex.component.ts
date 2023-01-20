0929e5b408890e93c69cf0cad89a371881f753fd
jabed <jabzeeshan.1009@gmail.com>
2023-01-21 00:27:07 +0530

    practice flex

---
+++ prac1/src/app/flex/flex.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './flex.component.html',
+    styleUrls: ['./flex.component.scss']
+})
+export class FlexComponent {
+    title = 'frontEndPrac';
+
+
+    gridContent: any = {
+
+    }
+}
