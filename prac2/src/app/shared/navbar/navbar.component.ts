ba1e7306a3076b99864d68a1d2d37a61bae7f10f
jabed <jabzeeshan.1009@gmail.com>
2023-01-24 14:40:35 +0530

    added navbar and sidebar

--- prac2/src/app/shared/navbar/navbar.component.ts
+++ prac2/src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit
+{
+	navbarButtons: string[] = ["Home", "About Us", "Gallery", "Trips"]
+
+	ngOnInit(): void
+	{
+
+	}
+
+	redirectTo()
+	{
+		console.log("Buttons are working!! :)")
+	}
 
 }
--- prac2/src/app/shared/sidebar/sidebar.component.ts
+++ prac2/src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+	selector: 'app-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit
+{
+	collapsed: boolean = true;
+
+	ngOnInit(): void
+	{
+
+	}
+
+	toggleSidebar()
+	{
+		this.collapsed = !this.collapsed;
+	}
 
 }
